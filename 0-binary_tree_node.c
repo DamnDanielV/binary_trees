453dae0d1e0552dab2b50f14260dcae4f6ee59b1
DamnDanielV <1821@holbertonschool.com>
2020-11-24 18:26:47 -0500

    fixed betty and added prototypes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,22 +1,22 @@
 #include "binary_trees.h"
-/*
-* binary_tree_t - creates a binary tree node
+/**
+* binary_tree_node - function thats creates a binary tree node
 * @parent: pointer to the parent node of the node to create
 * @value: value to put in the new node
-* return a pointer to the new node
+* Return: a pointer to the new node
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_leaf_node;
-    
-    new_leaf_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_leaf_node;
 
-    if (new_leaf_node)
-    {
-        new_leaf_node->parent = parent;
-        new_leaf_node->right = NULL;
-        new_leaf_node->left = NULL;
-        new_leaf_node->n = value;
-    }
-    return (new_leaf_node);
+	new_leaf_node = malloc(sizeof(binary_tree_t));
+
+	if (new_leaf_node)
+	{
+		new_leaf_node->parent = parent;
+		new_leaf_node->right = NULL;
+		new_leaf_node->left = NULL;
+		new_leaf_node->n = value;
+	}
+	return (new_leaf_node);
 }
